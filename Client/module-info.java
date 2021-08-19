4531d8167f5e3ae196d0c2cdf5b3bdf49ded30e6
Tasmia Zerin <60105612+TasmiaZerin1128@users.noreply.github.com>
2021-08-20 00:59:59 +0600

    Added Questions

---
+++ Client/module-info.java
@@ -0,0 +1,9 @@
+module SoftwareProjectLab {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires jdk.javadoc;
+
+    opens fusics;
+}
